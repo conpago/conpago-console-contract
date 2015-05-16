b0492d01fbaa608748fd9f7364846272ecead142
Bartosz Golek <bartosz.golek@gmail.com>
2015-05-16 23:41:09 +0200

    IConsolePresenter

---
+++ src/Saigon/Conpago/Console/Contract/Presentation/IConsolePresenter.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bg
+	 * Date: 16.05.15
+	 * Time: 23:38
+	 */
+
+	namespace Saigon\Conpago\Console\Contract\Presentation;
+
+
+	interface IConsolePresenter {
+		public function write($string);
+	}
