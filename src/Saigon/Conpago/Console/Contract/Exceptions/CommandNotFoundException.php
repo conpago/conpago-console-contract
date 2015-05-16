5ab668d52105a9f1f815cc7be1e4928472e74994
Bartosz Golek <bartosz.golek@gmail.com>
2015-05-16 20:56:28 +0200

    exception in contract

---
+++ src/Saigon/Conpago/Console/Contract/Exceptions/CommandNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bg
+	 * Date: 15.05.15
+	 * Time: 21:06
+	 */
+
+	namespace Saigon\Conpago\Console\Contract\Exceptions;
+
+	use Saigon\Conpago\Exception;
+
+	class CommandNotFoundException extends Exception {
+
+	}
