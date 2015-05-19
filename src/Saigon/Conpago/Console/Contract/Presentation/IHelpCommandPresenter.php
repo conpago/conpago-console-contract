b4976c735cc653516dd7661bb2960c5c7a8fea70
Bartosz Golek <bartosz.golek@gmail.com>
2015-05-20 00:15:22 +0200

    IHelpCommandPresenter

---
+++ src/Saigon/Conpago/Console/Contract/Presentation/IHelpCommandPresenter.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bg
+	 * Date: 18.05.15
+	 * Time: 00:37
+	 */
+
+	namespace Saigon\Conpago\Console\Contract\Presentation;
+
+
+	interface IHelpCommandPresenter {
+
+		public function printCommandInfo( $command, $desc );
+	}
